d1f92acc4e884ddd04c3f6bcec29c183941d65d0
luyandamagava <57810275+luyandamagava@users.noreply.github.com>
2023-07-27 18:20:08 -0700

    -added the abiility to added a new post -created array to show all the posts

--- network/views.py
+++ network/views.py
@@ -4,11 +4,16 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Comment, Post#, follower, following
 
 
 def index(request):
-    return render(request, "network/index.html")
+    user = request.user
+    posts = user.creator_posts.all()
+
+    return render(request, "network/index.html", {
+        "posts": posts, 
+    })
 
 
 def login_view(request):
@@ -61,3 +66,12 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+    
+def new_post(request):
+    if request.method == "POST":
+        content = request.POST["new_post_text"]
+        post = Post(creator=request.user, content=content)
+        post.save()
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/new_post.html")
